0065aec9e742beba3f2c07e20db9c0345dbe69a0
Giacomo Tazzari <giacomo@tazzari.dev>
2025-04-28 15:32:14 +0000

    Fix extra parenthesis in analysis string

--- suomi_study_helper/main.py
+++ suomi_study_helper/main.py
@@ -25,7 +25,7 @@ class AmbiguousTokenAnalysis:
     analyses: list[str]
 
     def __str__(self):
-        return f"{self.token} = {self.analyses})"
+        return f"{self.token} = {self.analyses}"
 
 @dataclasses.dataclass
 class UnambiguosTokenAnalysis:
